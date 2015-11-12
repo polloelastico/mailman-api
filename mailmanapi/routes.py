1e50567353c888cb74363a098cb76a349a50c315
JeffBackwards <jeffrey.t.edwards@gmail.com>
2015-11-12 12:08:43 -0600

    Started work on apiv3, a more strictly restful version with more functions.

--- mailmanapi/routes.py
+++ mailmanapi/routes.py
@@ -2,6 +2,7 @@
 from bottle import default_app
 from . import apiv1
 from . import apiv2
+from . import apiv3
 
 
 def create_routes(app):
@@ -18,7 +19,7 @@ def create_routes(app):
     app.route('/<listname>/sendmail', method='POST',
               callback=apiv1.sendmail)
 
-    # v2
+    # v2 this isnt' restful!  blech
     app.route('/v2/lists/', method='GET',
               callback=apiv2.list_lists)
     app.route('/v2/lists/<listname>', method='POST',
@@ -31,6 +32,24 @@ def create_routes(app):
               callback=apiv2.members)
     app.route('/v2/sendmail/<listname>', method='POST',
               callback=apiv2.sendmail)
+    
+    # v3
+    app.route('/', method='GET',
+              callback=apiv3.list_lists)
+    app.route('/<listname>', method='PUT',
+              callback=apiv3.create_list)
+    app.route('/<listname>', method='DELETE',
+              callback=apiv3.delete_list)
+    app.route('/<listname>', method='GET',
+              callback=apiv3.list_attr)
+    app.route('/<listname>/', method='POST',
+              callback=apiv3.sendmail)
+    app.route('/<listname>/members', method='PUT',
+              callback=apiv3.subscribe)
+    app.route('/<listname>/members', method='DELETE',
+              callback=apiv3.unsubscribe)
+    app.route('/<listname>/members', method='GET',
+              callback=apiv3.members)
 
 
 def get_application(allowed_ips):
